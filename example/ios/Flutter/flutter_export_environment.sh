86ce11c99af8518b90bbba4bc5930a0760ffd195
brucehan.xiao <brucehan.xiao@idreamsky.com>
2022-11-08 20:11:44 +0800

    feat: iOSSDK修改为自定义UI版本

--- example/ios/Flutter/flutter_export_environment.sh
+++ example/ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=/Users/vke/fvm/versions/2.0.3"
-export "FLUTTER_APPLICATION_PATH=/Users/vke/Work/jverify-flutter-plugin/example"
+export "FLUTTER_ROOT=/Users/brucehan.xiao/fvm/versions/2.10.5"
+export "FLUTTER_APPLICATION_PATH=/Users/brucehan.xiao/Desktop/jverify-flutter-plugin/example"
+export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build/ios"
-export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NAME=1.6.66"
 export "FLUTTER_BUILD_NUMBER=1"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=false"
