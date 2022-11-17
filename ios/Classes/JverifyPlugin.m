77e2a6c1461125b1c2999f32cac1d4e5e124e959
brucehan.xiao <brucehan.xiao@idreamsky.com>
2022-11-17 15:58:24 +0800

    fix: 修复iOS漏掉清除缓存方法

--- ios/Classes/JverifyPlugin.m
+++ ios/Classes/JverifyPlugin.m
@@ -43,7 +43,8 @@ NSObject<FlutterPluginRegistrar>* _jv_registrar;
         [self getSMSCode:call result:result];
     }else if ([methodName isEqualToString:@"setGetCodeInternal"]){
         [self setGetCodeInternal:call result:result];
-    }
+    }else if([methodName isEqualToString:@"clearPreLoginCache"])
+        [self clearPreLoginCache:call result:result];
     else {
         result(FlutterMethodNotImplemented);
     }
